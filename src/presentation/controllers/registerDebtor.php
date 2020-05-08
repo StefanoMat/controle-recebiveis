2545adc674055d3d1d4bdbfc70a2b9cd1a36792d
stefanoMat <stefanokaefer@gmail.com>
2020-05-07 22:19:05 -0300

    feat: returns 200 status code and register if valid data provided

--- src/presentation/controllers/registerDebtor.php
+++ src/presentation/controllers/registerDebtor.php
@@ -3,10 +3,18 @@ declare(strict_types=1);
 
 namespace Presentation\Controllers;
 
+use Domain\Models\Debtor;
+use Domain\UseCase\AddDebtor;
 use Presentation\Errors\MissingParamError;
 use Presentation\Protocols\Controller;
 
 class RegisterDebtor implements Controller{
+  private $addDebtorIm;
+
+  public function __construct(AddDebtor $addDebtor)
+  {
+    $this->addDebtorIm = $addDebtor;
+  }
 
   public function handle($htppRequest)
   {
@@ -17,6 +25,13 @@ class RegisterDebtor implements Controller{
         return new MissingParamError("Missing ".$field);
       }
     }
+    $debtorFields = $htppRequest['body'];
+    $register = $this->addDebtorIm->add(new Debtor($debtorFields['name'], $debtorFields['cpfCnpj'], $debtorFields['birthdate'], $debtorFields['address']));
+    print_r($register);
+    return [
+      'statusCode' => 200,
+      'body' => $register
+    ];
 
     } catch(\Error $e) {
 
