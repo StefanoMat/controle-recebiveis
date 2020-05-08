7237c011e4324a2a69a580dee9d4acb31a58ce13
stefanoMat <stefanokaefer@gmail.com>
2020-05-08 17:32:44 -0300

    style: details of code

--- src/presentation/controllers/registerDebtor.php
+++ src/presentation/controllers/registerDebtor.php
@@ -19,20 +19,18 @@ class RegisterDebtor implements Controller{
   public function handle($htppRequest)
   {
     try{
-    $requiredFields = ['name','cpfCnpj','birthdate', 'address'];
-    foreach ($requiredFields as $field) {
-      if(!isset($htppRequest['body'][$field])) {
-        return new MissingParamError("Missing ".$field);
+      $requiredFields = ['name','cpfCnpj','birthdate', 'address'];
+      foreach ($requiredFields as $field) {
+        if(!isset($htppRequest['body'][$field])) {
+          return new MissingParamError("Missing ".$field);
+        }
       }
-    }
-    $debtorFields = $htppRequest['body'];
-    $register = $this->addDebtorIm->add(new Debtor($debtorFields['name'], $debtorFields['cpfCnpj'], $debtorFields['birthdate'], $debtorFields['address']));
-    print_r($register);
-    return [
-      'statusCode' => 200,
-      'body' => $register
-    ];
-
+      $debtorFields = $htppRequest['body'];
+      $register = $this->addDebtorIm->add(new Debtor($debtorFields['name'], $debtorFields['cpfCnpj'], $debtorFields['birthdate'], $debtorFields['address']));
+      return [
+        'statusCode' => 200,
+        'body' => $register
+      ];
     } catch(\Error $e) {
 
     }
