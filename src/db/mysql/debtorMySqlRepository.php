84a8456fd3df60a7dc20b6d229e1ab08c7d22ca0
stefanoMat <stefanokaefer@gmail.com>
2020-05-08 17:30:59 -0300

    feat: DebtorMysqlRepository created to add a Debtor

---
+++ src/db/mysql/debtorMySqlRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+namespace Db\Mysql;
+
+use Domain\Models\Debtor;
+use Domain\Usecase\AddDebtor;
+use Db\Mysql\MysqlHelper;
+
+class DebtorMySqlRepository implements AddDebtor{
+
+  private $db;
+
+  public function __construct()
+  {
+    $pdo = new MysqlHelper();
+    $this->db = $pdo->getConn();
+  }
+
+  public function add(Debtor $debtor)
+  {
+    $queryToExecute = $this->db->prepare('insert into debtor (name, cpf_cnpj, birthdate, address) values (:name, :cpfCnpj, :birthdate, :address)');
+    $debtorData = $this->__debtorData($debtor);
+
+    $queryToExecute->bindParam(':name',$debtorData['name']);
+    $queryToExecute->bindParam(':cpfCnpj', $debtorData['cpfCnpj']);
+    $queryToExecute->bindParam(':birthdate', $debtorData['birthdate']);
+    $queryToExecute->bindParam(':address', $debtorData['address']);
+    $queryToExecute->execute();
+    return $this->db->lastInsertId();
+  }
+
+  private function __debtorData(Debtor $debtor) {
+    return [
+      'name' => $debtor->getName(),
+      'cpfCnpj' => $debtor->getCpfCnpj(),
+      'birthdate' => $debtor->getBirthdate(),
+      'address' => $debtor->getAddress()
+    ];
+  }
+}
