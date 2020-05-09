6b6c34fc937c1ef6431de8b83f96944b277fac32
stefanoMat <stefanokaefer@gmail.com>
2020-05-09 04:18:59 -0300

    test: should return MissingParamError if no value or no endDate is provided

--- tests/presentation/controllers/registerDebtTest.php
+++ tests/presentation/controllers/registerDebtTest.php
@@ -19,4 +19,32 @@ class RegisterDebtTest extends TestCase{
       $response = $sut->handle($httpRequest);
       $this->assertEquals($response->getBody(), new MissingParamError("debtTitle"));
   }
+
+  public function testReturnsErrorIfNoValueProvided()
+  {
+    $sut = new RegisterDebt();
+    $httpRequest = [
+      'body' => [
+        'debtTitle' => 'valid_title',
+        'endDate' => 'valid_date',
+        ] 
+      ];
+
+      $response = $sut->handle($httpRequest);
+      $this->assertEquals($response->getBody(), new MissingParamError("value"));
+  }
+
+  public function testReturnsErrorIfNoEndDateProvided()
+  {
+    $sut = new RegisterDebt();
+    $httpRequest = [
+      'body' => [
+        'value' => 'valid_value',
+        'debtTitle' => 'valid_title',
+        ] 
+      ];
+
+      $response = $sut->handle($httpRequest);
+      $this->assertEquals($response->getBody(), new MissingParamError("endDate"));
+  }
 }
