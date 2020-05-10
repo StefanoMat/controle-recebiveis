caee2f45a220a4fcb2e9ed8daa03816ce91e8786
stefanoMat <stefanokaefer@gmail.com>
2020-05-10 01:00:34 -0300

    feat: cpjcnpj mask with ctrl+c correctly

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,16 @@ $(document).ready(function () {
   $('.date').mask("00/00/0000", { placeholder: "__/__/____" });
   $('.money').mask('000.000.000.000.000,00', { reverse: true });
 
+  var mask = '00.000.000/0000-00';
 
+  $('.cpfcnpj').mask(mask, {
+    onKeyPress: function (val, e, field, options) {
+      var masks = ['000.000.000-009', '00.000.000/0000-00'];
+      mask = (val.length <= 14) ? masks[0] : masks[1];
+      if (val.length == 0) {
+        mask = '00.000.000/0000-00';
+      }
+      field.mask(mask, options);
+    }
+  });
 })
