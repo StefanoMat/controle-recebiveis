c53fb527045e3acbce122c9fa6147ac910fda630
stefanoMat <stefanokaefer@gmail.com>
2020-05-10 08:29:25 -0300

    refactor: change the RemoveReceivable's first test

--- app/tests/presentation/controllers/removeReceivableTest.php
+++ app/tests/presentation/controllers/removeReceivableTest.php
@@ -1,17 +1,35 @@
 <?php
 
+use Domain\Usecase\DeleteReceivables;
 use Presentation\Errors\MissingParamError;
 use \PHPUnit\Framework\TestCase;
 use Presentation\Controllers\RemoveReceivable;
 
+class DeleteReceivableStub implements DeleteReceivables{
+  public function delete($debtorId, $debtId)
+  {
+    $register = [
+        'id' => 'valid_id',
+        'name' => 'valid_name',
+        'cpfCnpj' => 0,
+        'birthdate' => 'valid_birthdate',
+        'address' => 'valid_address',
+        'value' => 'valid_value',
+        'debtDescription' => 'valid_description',
+        'endDate' => 'valid_date'
+    ];
+    return $register;
+  }
+}
+
 class RemoveReceivableTest extends TestCase{
 
-  public function testReturnsErrorIfNoIdProvided()
+  public function testReturnsErrorIfNoDebtorIdProvided()
   {
-    $sut = new RemoveReceivable();
+    $sut = new RemoveReceivable(new DeleteReceivableStub());
 
     $httpRequest = [];
     $response = $sut->handle($httpRequest);
-    $this->assertEquals($response->getBody(), new MissingParamError("id"));
+    $this->assertEquals($response->getBody(), new MissingParamError("debtorId"));
   }
 }
