8702a4c86d4da48a90c09d6168689f3ca8e6a7a5
stefanoMat <stefanokaefer@gmail.com>
2020-05-10 00:59:49 -0300

    feat: informations and fields added

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -25,6 +25,7 @@ require BASE.'/views/essentials/head.php';
     </div>
 
     <div class="col-md-7 list">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@novo">Novo Recebível</button>
       <div class="list-box">
         <div class="line-box">
           <div class="field col-sm-2"><p>Nome</p>Stefano Kaefer</div>
@@ -54,6 +55,54 @@ require BASE.'/views/essentials/head.php';
    
   </div> -->
 </section>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Novo Recebível</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-nome" class="col-form-label">Nome:</label>
+            <input type="text" name="nome" class="form-control" id="recipient-nome">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">CPF/CNPJ:</label>
+            <input type="text" name="cpfcnpj" class="form-control cpfcnpj" id="recipient-cpfcnpj">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Data de Nascimento:</label>
+            <input type="text" name="data_nascimento" class="form-control date" id="recipient-nascimento">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Endereço:</label>
+            <input type="text" name="endereco" class="form-control" id="recipient-endereco">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Descrição do título:</label>
+            <textarea name="descricao" class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Valor:</label>
+            <input type="text" name="valor" class="form-control money" id="recipient-valor">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Data de Vencimento:</label>
+            <input type="text" name="data_vencimento" class="form-control date" id="recipient-final">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Criar</button>
+      </div>
+    </div>
+  </div>
+</div>
 <?php
 require BASE.'/views/essentials/footer.php';
 ?>
